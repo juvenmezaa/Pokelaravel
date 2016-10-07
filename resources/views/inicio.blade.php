15ebb84b64b96e06d0019f6ddb28000c63985056
pebblead <pebble.ad@hotmail.com>
2016-10-07 03:19:17 -0600

    jumbotron

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -8,9 +8,11 @@
 			<img src="img/hola.png" alt="Bienvenido" style="margin-left:-70px; margin-top: -10px; width:300px;">
 		</div>
 		<div class="col-sm-offset-3">
-			<h1>Poképroyecto Laravel</h1>
+
+			<img src="img/pokeproyecto.png" alt="" width="80%" style="margin-left: 65px;">
 	  		<p>Este sistema es el resultado de utilizar el Framework Laravel en su versión 5.2 para gestionar Pokémon de diversos tipos. Logrando aprender un poco de estas peculiares creaturas.</p>
-	  		<p><a class="btn btn-primary btn-lg" href="{{url('')}}">Comenzar</a></p>
+	  		<p><a class="btn btn-primary btn-lg pull-right" href="{{url('')}}">Comenzar</a></p>
+	  		<br>
 		</div>
 	  
 	</div>
