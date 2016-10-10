64aa78550e9b73adb5a82683f6f9dc89c9d97223
pebblead <pebble.ad@hotmail.com>
2016-10-10 01:14:34 -0600

    info pokemon

--- resources/views/pokemoninfo.blade.php
+++ resources/views/pokemoninfo.blade.php
@@ -6,7 +6,7 @@
 <figure class="figpokeinfo">
 	<div id="tituloinfo"><h2><span class="titulopok">{{$pokemon->nombre}}</span></h2></div>
 	<section class="info_pokemon">
-		<article><img src="../img/{{$pokemon->imagen}}" alt=""></article>
+		<article><img src="../img/{{$pokemon->imagen}}" alt="{{$pokemon->nombre}}"></article>
 		<article>
 			<div>datos1</div>
 			<div class="datos">datos 2</div>
