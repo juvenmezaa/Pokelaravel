1ed3c4950ce4c4c9bc94f18f8125d6f1444ec3e0
pebblead <pebble.ad@hotmail.com>
2016-10-10 01:43:09 -0600

    informacion de un pokemon

--- resources/views/pokemoninfo.blade.php
+++ resources/views/pokemoninfo.blade.php
@@ -3,14 +3,15 @@
 <h2></h2>
 @stop
 @section("contenido")
+<div class="display">
 <figure class="figpokeinfo">
 	<div id="tituloinfo"><h2><span class="titulopok">{{$pokemon->nombre}}</span></h2></div>
 	<section class="info_pokemon">
 		<article><img src="../img/{{$pokemon->imagen}}" alt="{{$pokemon->nombre}}" width="90%"></article>
 		<article>
-			<div>datos1</div>
+			<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus debitis cupiditate nisi dolore aut dolor quae officia vitae ad accusamus tempora fugiat ullam molestias eius laborum hic similique beatae, officiis.</div>
 			<div class="datos">
-				<table id="tblinfo" width="100%">
+				<table id="tblinfopk" width="100%">
 					<tr>
 						<th class="sptd">Numero</th>
 						<th class="sptd">Peso</th>
@@ -28,7 +29,7 @@
 						<td>{{$pokemon->altura}}</td>
 						<td>{{$pokemon->pc}}</td>
 					</tr>
-					<tr>
+				<tr>
 						<th class="sptd">PS</th>
 						<th class="sptd">Polvos</th>
 					</tr>
@@ -41,5 +42,9 @@
 			</div>
 		</article>
 	</section>
+	<div id="divtipos">
+		<a href="{{url('pdfPokemon')}}/{{$pokemon->id}}"><span class="glyphicon glyphicon-download-alt"></span></a>
+	</div>
 </figure>
+</div>	
 @stop
